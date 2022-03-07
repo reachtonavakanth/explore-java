508467826bcdcb98b1da0fa49fe4d66c0d89e98d
navakanth <navakanthnavi@navakanths-MacBook-Pro.local>
2022-03-07 06:51:23 +0530

    Data Type added

---
+++ src/com/mycomp/basics/StringDataHolders.java
@@ -0,0 +1,13 @@
+package com.mycomp.basics;
+
+public class StringDataHolders {
+    public static void main(String[] args) {
+
+    }
+
+    public static void holdString(){
+        String str1= "I am a string";
+
+    }
+
+}
